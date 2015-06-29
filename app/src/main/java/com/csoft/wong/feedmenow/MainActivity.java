a10b9fd648dd9e9b644ae06b315e80bc85a7e6ee
speedwhy <lukas.walker@bluewin.ch>
2015-06-29 10:15:02 +0200

    isolated asynctask

--- app/src/main/java/com/csoft/wong/feedmenow/MainActivity.java
+++ app/src/main/java/com/csoft/wong/feedmenow/MainActivity.java
@@ -77,41 +77,10 @@ public class MainActivity extends ActionBarActivity {
             }
         }*/
 
-        new AsyncTask<String, String, String>(){
-            @Override
-            protected String doInBackground(String... url) {
-                APIBinder apiBinder = new APILoader();
-                /*
-                String jsonstring = apiBinder.readUrl(url[0]);
-                return jsonstring;
-                */
-                String xmlstring = apiBinder.readUrl(url[0]);
-                return xmlstring;
-            }
-
-            protected void onPostExecute(String xmlstring) {
-                APIBinder apiBinder = new APILoader();
-                HashMap<String, HashMap<String, String>> allResults = apiBinder.parseXml(xmlstring);
-
-                /*ArrayList ing_list = new ArrayList();
-                for (int i = 0; i < allResults.size()-1;i++){
-                    ing_list.add(allResults.get(Integer.toString(i)).get("title"));
-                }*/
-
-
-                Intent intent;
-                intent = new Intent(getApplicationContext(), Recipe_Activity.class);
-                //intent.putStringArrayListExtra("ingredients", ing_list);
-                intent.putExtra("title", allResults.get(Integer.toString(0)).get("title"));
-                intent.putExtra("ingredients", allResults.get(Integer.toString(0)).get("ingredients").split(", "));
-                intent.putExtra("thumbnail", allResults.get(Integer.toString(0)).get("thumbnail"));
-                intent.putExtra("href", allResults.get(Integer.toString(0)).get("resultHref"));
-                startActivity(intent);
-            }
-
-        }.execute(url);
-
-
+        Intent intent;
+        intent = new Intent(getApplicationContext(), Recipe_Activity.class);
+        readURLAsyncTask urlAsyncTask = new readURLAsyncTask(intent, this);
+        urlAsyncTask.execute(url);
 
     }
 
