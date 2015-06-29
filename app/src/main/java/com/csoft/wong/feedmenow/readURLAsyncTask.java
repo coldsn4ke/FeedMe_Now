68e5c5b70e6a0308f32d083cb4406537fdc64bde
speedwhy <lukas.walker@bluewin.ch>
2015-06-29 10:11:36 +0200

    added readURLAsyncTask

---
+++ app/src/main/java/com/csoft/wong/feedmenow/readURLAsyncTask.java
@@ -0,0 +1,49 @@
+package com.csoft.wong.feedmenow;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import java.util.HashMap;
+
+
+public class readURLAsyncTask extends AsyncTask<String, String, String> {
+
+    private Intent intent;
+    private Context context;
+
+    public readURLAsyncTask(Intent intent, Context context){
+        this.context = context;
+        this.intent = intent;
+    }
+
+    @Override
+    protected String doInBackground(String... url) {
+        APIBinder apiBinder = new APILoader();
+        /*
+        String jsonstring = apiBinder.readUrl(url[0]);
+        return jsonstring;
+        */
+        String xmlstring = apiBinder.readUrl(url[0]);
+        return xmlstring;
+    }
+
+    protected void onPostExecute(String xmlstring) {
+        APIBinder apiBinder = new APILoader();
+        HashMap<String, HashMap<String, String>> allResults = apiBinder.parseXml(xmlstring);
+
+                /*ArrayList ing_list = new ArrayList();
+                for (int i = 0; i < allResults.size()-1;i++){
+                    ing_list.add(allResults.get(Integer.toString(i)).get("title"));
+                }*/
+
+        //intent.putStringArrayListExtra("ingredients", ing_list);
+        intent.putExtra("title", allResults.get(Integer.toString(0)).get("title"));
+        intent.putExtra("ingredients", allResults.get(Integer.toString(0)).get("ingredients").split(", "));
+        intent.putExtra("thumbnail", allResults.get(Integer.toString(0)).get("thumbnail"));
+        intent.putExtra("href", allResults.get(Integer.toString(0)).get("resultHref"));
+        context.startActivity(intent);
+        //((Activity)context).finish();
+    }
+}
