dc4956eaed5c93be9e5022a67645b75714103a80
speedwhy <lukas.walker@bluewin.ch>
2015-06-30 14:54:23 +0200

    added shake functionality to search

--- app/src/main/java/com/csoft/wong/feedmenow/MainActivity.java
+++ app/src/main/java/com/csoft/wong/feedmenow/MainActivity.java
@@ -1,6 +1,9 @@
 package com.csoft.wong.feedmenow;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.app.ActionBar;
 import android.content.Intent;
@@ -42,6 +45,10 @@ public class MainActivity extends ActionBarActivity {
     private APIBinder apiBinder;
     private EditText ingredient;
 
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +58,39 @@ public class MainActivity extends ActionBarActivity {
         edit = (EditText) findViewById(R.id.ing0);
         count = 0;
         apiBinder = new APILoader();
+
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+                 * method you would use to setup whatever you want done once the
+                 * device has been shook.
+                 */
+                search(findViewById(R.id.search_btn));
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,    SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 
 
